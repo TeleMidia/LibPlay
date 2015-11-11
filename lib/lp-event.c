583009d464f94f11ee01a85489aa5b7b8a510832
Guilherme F. Lima <gflima@sdf.org>
2015-11-11 16:03:29 -0200

    lib, tests: Enable event module
    * lib/Makefile.am: Updated accordingly.
    * lib/lp-event.c: Renamed from lib/event.c; comments were improved.
    * tests/Makefile.am: Enable event module tests.

--- lib/lp-event.c
+++ lib/lp-event.c
@@ -21,8 +21,14 @@ along with LibPlay.  If not, see <http://www.gnu.org/licenses/>.  */
 #include "play.h"
 #include "macros.h"
 
+
+/* Exported functions.  */
+
 /*-
- * Initializes EVENT as a start event.
+ * lp_event_init_start:
+ * @event: a #lp_event_t
+ *
+ * Initializes @event as %LP_EVENT_START.
  */
 void
 lp_event_init_start (lp_event_t *event)
@@ -31,7 +37,10 @@ lp_event_init_start (lp_event_t *event)
 }
 
 /*-
- * Initializes EVENT as a stop event.
+ * lp_event_init_stop:
+ * @event: a #lp_event_t
+ *
+ * Initializes @event as %LP_EVENT_STOP.
  */
 void
 lp_event_init_stop (lp_event_t *event)
@@ -40,8 +49,14 @@ lp_event_init_stop (lp_event_t *event)
 }
 
 /*-
- * Returns true if events E1 and E2 are equal, i.e., if they have the same
- * type and carry identical data.  Otherwise, returns false.
+ * lp_event_equals:
+ * @e1: a #lp_event_t
+ * @e2: a #lp_event_t
+ *
+ * Checks whether events @e1 and @2 are equal, if they have the same
+ * type and carry identical data.
+ *
+ * Return value: %TRUE if successful, or %FALSE otherwise.
  */
 ATTR_PURE int
 lp_event_equals (const lp_event_t *e1, const lp_event_t *e2)

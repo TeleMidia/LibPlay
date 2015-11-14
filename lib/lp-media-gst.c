1a5a2f581d528de807bd272716025c4486827886
Guilherme F. Lima <gflima@sdf.org>
2015-11-14 21:21:47 -0200

    maint: Make GStreamer back-end functional
    * lib/lp-media-gst.c: Add some missing stuff.
    * Makefile.am (SUBDIRS): Add src.

--- lib/lp-media-gst.c
+++ lib/lp-media-gst.c
@@ -18,19 +18,9 @@ along with LibPlay.  If not, see <http://www.gnu.org/licenses/>.  */
 
 #include <config.h>
 
+#include "gstx-macros.h"
 #include "play.h"
 #include "play-internal.h"
-
-/* *INDENT-OFF* */
-PRAGMA_DIAG_PUSH ()
-PRAGMA_DIAG_IGNORE (-Wcast-align)
-PRAGMA_DIAG_IGNORE (-Wcast-qual)
-PRAGMA_DIAG_IGNORE (-Wconversion)
-PRAGMA_DIAG_IGNORE (-Wpedantic)
-PRAGMA_DIAG_IGNORE (-Wvariadic-macros)
-#include <gst/gst.h>
-PRAGMA_DIAG_POP ()
-/* *INDENT-ON* */
 
 /* GStreamer back-end data.  */
 typedef struct _lp_media_gst_t
@@ -42,6 +32,7 @@ typedef struct _lp_media_gst_t
 /* *INDENT-OFF* */
 static lp_media_gst_t *__lp_media_gst_check (const lp_media_t *);
 static GstPipeline *__lp_media_gst_get_pipeline (lp_media_t *);
+static gboolean __lp_gst_media_pipeline_bus_async_callback (GstBus *, GstMessage *, gpointer);
 /* *INDENT-ON* */
 
 /* Checks and returns the back-end data associated with @media.  */
@@ -57,19 +48,111 @@ __lp_media_gst_check (const lp_media_t *media)
 /* Gets #GstPipeline associated with @media.  Returns a #GstPipeline if
    successful, or %NULL if @media has no associated #GstPipeline.  */
 
-static ATTR_UNUSED GstPipeline *
+static GstPipeline *
 __lp_media_gst_get_pipeline (lp_media_t *media)
 {
+  lp_media_t *root = _lp_media_get_root_ancestor (media);
+  lp_media_gst_t *gst = __lp_media_gst_check (root);
+  return gst->pipeline;
+}
+
+/* Allocates and installs a new #GstPipeline to @media.  Returns %TRUE if
+   successful, or %FALSE if @media already has an associated
+   #GstPipeline.  */
+
+static lp_bool_t
+__lp_media_gst_install_pipeline (lp_media_t *media)
+{
+  lp_media_t *root;
   lp_media_gst_t *gst;
 
-  gst = __lp_media_gst_check (media);
+  GstPipeline *pipeline;
+  GstBus *bus;
+
+  root = _lp_media_get_root_ancestor (media);
+  gst = __lp_media_gst_check (root);
   if (gst->pipeline != NULL)
-    return gst->pipeline;
+    return FALSE;
 
-  if (media->parent != NULL)
-    return __lp_media_gst_get_pipeline (media->parent);
+  pipeline = GST_PIPELINE (gst_pipeline_new ("pipeline"));
+  _lp_assert (pipeline != NULL);
 
-  return NULL;
+  bus = gst_pipeline_get_bus (pipeline);
+  _lp_assert (bus != NULL);
+  gst_bus_add_watch (bus, __lp_gst_media_pipeline_bus_async_callback, NULL);
+  g_object_unref (bus);
+
+  g_object_set_data (G_OBJECT (pipeline), "lp_media", root);
+  gst->pipeline = pipeline;
+  return TRUE;
+}
+
+/* Requests an asynchronous start of @media.
+   Returns %TRUE if successful, or %FALSE if @media cannot start.  */
+
+static lp_bool_t
+__lp_media_gst_start_async (lp_media_t *media)
+{
+  GstStateChangeReturn status;
+  GstPipeline *pipeline;
+
+  pipeline = __lp_media_gst_get_pipeline (media);
+  if (pipeline == NULL)
+    {
+      _lp_assert (__lp_media_gst_install_pipeline (media));
+      pipeline = __lp_media_gst_get_pipeline (media);
+      _lp_assert (pipeline != NULL);
+    }
+
+  status = gst_element_set_state (GST_ELEMENT (pipeline),
+                                  GST_STATE_PLAYING);
+  if (unlikely (status == GST_STATE_CHANGE_FAILURE))
+    return FALSE;
+
+  return TRUE;
+}
+
+/************************** GStreamer callbacks ***************************/
+
+/* Handles #GstBus asynchronous messages.  */
+
+static gboolean
+__lp_gst_media_pipeline_bus_async_callback (arg_unused (GstBus *bus),
+                                            GstMessage *message,
+                                            arg_unused (gpointer data))
+{
+  GstObject *obj;
+
+  gstx_dump_message (message);
+
+  obj = GST_MESSAGE_SRC (message);
+  _lp_assert (obj != NULL);
+
+  switch (GST_MESSAGE_TYPE (message))
+    {
+    case GST_MESSAGE_STATE_CHANGED:
+      {
+        GstState new_state;
+        lp_media_t *media;
+        lp_event_t event;
+
+        media = (lp_media_t *) g_object_get_data (G_OBJECT (obj),
+                                                  "lp_media");
+        if (media == NULL)
+          break;
+
+        gst_message_parse_state_changed (message, NULL, &new_state, NULL);
+        if (new_state != GST_STATE_PLAYING)
+          break;
+
+        lp_event_init_start (&event);
+        _lp_media_dispatch (media, &event);
+      }
+    default:
+      break;
+    }
+
+  return TRUE;
 }
 
 /*************************** Back-end callbacks ***************************/
@@ -92,10 +175,12 @@ __lp_media_gst_post_func (lp_media_t *media, lp_event_t *event)
 
   switch (event->type)
     {
+    case LP_EVENT_START:
+      __lp_media_gst_start_async (media);
+      break;
     case LP_EVENT_USER:
       _lp_media_dispatch (media, event);
       break;
-    case LP_EVENT_START:
     case LP_EVENT_STOP:
     default:
       _LP_ASSERT_NOT_REACHED;
@@ -112,6 +197,9 @@ void
 _lp_media_gst_init (lp_media_t *media)
 {
   lp_media_gst_t *gst;
+
+  if (!gst_is_initialized ())
+    gst_init (NULL, NULL);
 
   _lp_assert (media != NULL);
   _lp_assert (media->backend.data == NULL);

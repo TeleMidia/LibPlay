7cb9c93369129d160115f64380fe715fc0982a56
Guilherme F. Lima <gflima@sdf.org>
2015-11-14 17:42:39 -0200

    lib: Add comments
    * lib/lp-version.c: Likewise.

--- lib/lp-version.c
+++ lib/lp-version.c
@@ -20,12 +20,27 @@ along with LibPlay.  If not, see <http://www.gnu.org/licenses/>.  */
 
 #include "play.h"
 
+/*-
+ * lp_version:
+ *
+ * Returns the version of LibPlay encoded in a single integer.
+ * The encoding ensures that later version generate bigger numbers.
+ *
+ * Return value: the encoded version.
+ */
 int
 lp_version (void)
 {
   return LP_VERSION;
 }
 
+/*-
+ * lp_version_string:
+ *
+ * Returns the version of LibPlay as a human-readable string.
+ *
+ * Return value: a string containing the version.
+ */
 const char *
 lp_version_string (void)
 {

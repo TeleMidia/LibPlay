d3c5b1d3e863e2827b1ca705c3fbb4c63dccc4be
Rodrigo Costa <rodrigocosta@telemidia.puc-rio.br>
2016-04-10 15:30:10 -0300

    maint: Add option --with-syncclock to configure
    This option enables the use of a GstSynchronousClock object to
    deterministically control the pipeline's time. The macro HAVE_SYNCCLOCK can
    be used to test whether the library has support to this feature.
    
      * configure.ac
      * lib/Makefile.am
      * lib/play.h

--- lib/play.h
+++ lib/play.h
@@ -112,9 +112,6 @@ lp_event_init_user (lp_event_t *);
 LP_API lp_bool_t
 lp_event_equals (const lp_event_t *, const lp_event_t *);
 
-/* synchronous clock  */
-typedef struct _lp_sync_clock_t lp_sync_clock_t;
-
 /* media */
 
 typedef struct _lp_media_t lp_media_t;
@@ -184,10 +181,10 @@ lp_media_get_property_pointer (lp_media_t *, const char *, void **);
 LP_API lp_bool_t
 lp_media_set_property_pointer (lp_media_t *, const char *, const void *);
 
-LP_API lp_bool_t
-lp_media_set_sync_clock (lp_media_t *, lp_sync_clock_t *);
-
 /* synchronous clock  */
+
+#ifdef HAVE_SYNCCLOCK
+typedef struct _lp_sync_clock_t lp_sync_clock_t;
 
 LP_API lp_sync_clock_t *
 lp_sync_clock_create ();
@@ -206,6 +203,10 @@ lp_media_sync_clock_advance_time (lp_sync_clock_t *, uint64_t);
 
 LP_API void
 lp_media_sync_clock_tick_for (lp_sync_clock_t *, uint64_t);
+
+LP_API lp_bool_t
+lp_media_set_sync_clock (lp_media_t *, lp_sync_clock_t *);
+#endif
 
 LP_END_DECLS
 

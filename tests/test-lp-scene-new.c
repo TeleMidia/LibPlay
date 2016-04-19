e7d4c774e4f7acca9c4ff6828a7019513fc2457b
Rodrigo Costa <rodrigocosta@telemidia.puc-rio.br>
2016-04-19 16:48:49 -0300

    tests: Add new test   * tests/test-lp-scene-new.c

---
+++ tests/test-lp-scene-new.c
@@ -0,0 +1,31 @@
+/* Copyright (C) 2015 PUC-Rio/Laboratorio TeleMidia
+
+This file is part of LibPlay.
+
+LibPlay is free software: you can redistribute it and/or modify it
+under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+LibPlay is distributed in the hope that it will be useful, but WITHOUT
+ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public
+License for more details.
+
+You should have received a copy of the GNU General Public License
+along with LibPlay.  If not, see <http://www.gnu.org/licenses/>.  */
+
+#include "tests.h"
+
+int
+main (void)
+{
+  lp_scene_t *scene;
+
+  scene = g_object_new (LP_SCENE_T, "width", 1080, "height", 720, NULL); 
+  assert (scene != NULL);
+
+  g_object_unref (scene);
+
+  exit (EXIT_SUCCESS);
+}
